f14d11aa5a11c96e1069a03f26949f59ffd70326
Saulo Lima <roshi0021@gmail.com>
2018-04-04 21:22:13 -0300

    isolando rota do produto

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var app = require('./config/express')();
 var porta = 3000;
 
-app.get("/produtos", function(req, res){
-  console.log('Listando...');
-  res.render("produtos/lista");
-});
+var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(porta, function(){
   console.log("Server running at " + porta);
