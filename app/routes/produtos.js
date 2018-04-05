a585c99e0fb28f29ec09af8d4160b8743d567095
Saulo Lima <roshi0021@gmail.com>
2018-04-04 23:11:37 -0300

    preparando o driver mysql

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,8 @@
 module.exports = function(app){
   app.get("/produtos", function(req, res){
     console.log('Listando...');
+    var mysql = require('mysql');
+    
     res.render("produtos/lista");
   });
 }
