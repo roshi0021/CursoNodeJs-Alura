4eafb4cfb59db406b13dccb63f4d7bb220bfaa60
Saulo Lima <roshi0021@gmail.com>
2018-04-20 18:23:00 -0300

    Mais testes

--- test/produtos.js
+++ test/produtos.js
@@ -8,4 +8,18 @@ describe("#ProdutosController", function(){
       .expect('Content-type', /json/)
       .expect(200, done);
     });
+
+  it("#Cadastra produtos com dados invalidos", function(done) {
+    request.post("/produtos")
+    .send({titulo: "", descricao: "novo livro whatever"})
+    .expect(400, done)
+  });
+
+  it("#Cadastro produtos com dados válidos", function(done){
+    request.post("/produtos")
+    .send({titulo: "Livro Teste", descricao: "Novo livro para teste", preco: 110.98})
+    .expect(302, done);
+  });
+
+
   });
