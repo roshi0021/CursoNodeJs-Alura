448635f09b3dd083db11ddb151cf4fdde076ac35
Saulo Lima <roshi0021@gmail.com>
2018-04-26 17:09:18 -0300

    Preparando DB Heroku

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var app = require('./config/express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var porta = 3000;
+var porta = process.env.PORT || 3000;
 
 app.set('io', io);
 
-http.listen(porta, function(){
+var server = http.listen(porta, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
   console.log("Server running at " + porta);
 });
--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -17,6 +17,16 @@ function createDBConnection(){
         database: "casadocodigo_nodejs_test"
       });
   }
+  if(process.env.NODE_ENV == 'production'){
+    var url = process.env.CLEARDB_DATABASE_URL;
+    var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+    return mysql.createConnection({
+        host: "grupos[3]",
+        user: "grupos[1]",
+        password: "grupos[2]",
+        database: "grupos[4]"
+      });
+  }
 
 }
 
